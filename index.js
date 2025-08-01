5545ed64322122719a813857a63328b761f3e017
bigstock2001 <donald.l.dunn@gmail.com>
2025-08-01 10:57:50 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('Home page');
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '100px' }}>
+      <h1>Welcome to Barracks Media</h1>
+      <p>This is your new frontend. Customize this page anytime.</p>
+    </div>
+  );
+}
