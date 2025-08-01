b0434c29e508a543bc8936f25c9f32bb3b98f91b
bigstock2001 <donald.l.dunn@gmail.com>
2025-08-01 11:34:48 -0500

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ textAlign: 'center', marginTop: '100px' }}>
-      <h1>Welcome to Barracks Media</h1>
-      <p>This is your new frontend. Customize this page anytime.</p>
-    </div>
-  );
-}
