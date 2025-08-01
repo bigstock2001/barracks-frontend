316a3068c9b1d3971af6cf2ed524fd7ebd3abbf2
bigstock2001 <donald.l.dunn@gmail.com>
2025-08-01 11:26:02 -0500

    Create audiobooks.js

---
+++ pages/audiobooks.js
@@ -0,0 +1,8 @@
+export default function Audiobooks() {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>Audiobooks</h1>
+      <p>Listen to our growing catalog of audiobooks.</p>
+    </div>
+  );
+}
