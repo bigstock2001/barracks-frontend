e2404e5435a099341c5481af3a4496cf7374ec9c
bigstock2001 <donald.l.dunn@gmail.com>
2025-08-01 11:23:12 -0500

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>Welcome to Barracks Media</h1>
+      <p>This is your headless frontend powered by Next.js!</p>
+    </div>
+  );
+}
