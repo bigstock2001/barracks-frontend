fb1091f2be5ae68038a229bf90a841d63e302742
bigstock2001 <donald.l.dunn@gmail.com>
2025-08-01 11:26:39 -0500

    Create blog.js

---
+++ pages/blog.js
@@ -0,0 +1,8 @@
+export default function Blog() {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>Blog</h1>
+      <p>Stay up to date with Barracks Media news and updates.</p>
+    </div>
+  );
+}
