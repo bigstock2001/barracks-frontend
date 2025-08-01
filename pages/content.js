18c706a5535c60ed347393d399f0e0443438a3ac
bigstock2001 <donald.l.dunn@gmail.com>
2025-08-01 11:25:22 -0500

    Create content.js

---
+++ pages/content.js
@@ -0,0 +1,8 @@
+export default function Content() {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>Video Content</h1>
+      <p>Browse exclusive videos available to subscribers.</p>
+    </div>
+  );
+}
