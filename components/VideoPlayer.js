432b1b53855500f6576df1227d24bb0759a49201
bigstock2001 <donald.l.dunn@gmail.com>
2025-08-03 13:16:18 -0500

    update VideoPlayer.js

--- components/VideoPlayer.js
+++ components/VideoPlayer.js
@@ -1,21 +1,15 @@
 'use client';
 
-import { MuxPlayer } from '@mux/mux-player-react';
-
 export default function VideoPlayer({ playbackId }) {
-  console.log("Rendering MuxPlayer for playbackId:", playbackId);
-
   if (!playbackId) return <p>No video found.</p>;
 
   return (
     <div style={{ marginTop: '2rem' }}>
-      <MuxPlayer
-        playbackId={playbackId}
-        streamType="on-demand"
-        metadata={{
-          video_id: playbackId,
-          video_title: 'Barracks Media Video',
-        }}
+      <mux-player
+        playback-id={playbackId}
+        stream-type="on-demand"
+        metadata-video-id={playbackId}
+        metadata-video-title="Barracks Media Video"
         style={{
           width: '100%',
           height: 'auto',
@@ -23,7 +17,7 @@ export default function VideoPlayer({ playbackId }) {
           maxWidth: '800px',
           margin: '0 auto',
         }}
-      />
+      ></mux-player>
     </div>
   );
 }
