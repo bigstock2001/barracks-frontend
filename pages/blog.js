d006a85dc0c27c96aeb1d3f938e578262d45bcc9
bigstock2001 <donald.l.dunn@gmail.com>
2025-08-03 12:01:51 -0500

    updated file tree

--- pages/blog.js
+++
@@ -1,8 +0,0 @@
-export default function Blog() {
-  return (
-    <div style={{ padding: '2rem' }}>
-      <h1>Blog</h1>
-      <p>Stay up to date with Barracks Media news and updates.</p>
-    </div>
-  );
-}
--- pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
-      <h1>Welcome to Barracks Media</h1>
-      <p>This is your headless frontend powered by Next.js!</p>
-    </div>
-  );
-}
