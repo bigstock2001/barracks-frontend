1105ab5443d8010528f11998d38a43bf0bddb189
bigstock2001 <donald.l.dunn@gmail.com>
2025-08-02 19:07:12 -0500

    Update layout.js

--- pages/app/layout.js
+++ pages/app/layout.js
@@ -1,20 +1,4 @@
-// app/layout.js
 export const metadata = {
-  title: 'Barracks Media',
-  description: 'On-demand video platform',
+  title: "Barracks Media",
+  description: "On-demand video platform",
 };
-
-export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <head>
-        {/* Mux Player Script */}
-        <script
-          async
-          src="https://unpkg.com/@mux/mux-player"
-        ></script>
-      </head>
-      <body>{children}</body>
-    </html>
-  );
-}
