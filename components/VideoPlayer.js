fa5687e10d3b20fe3f6a24bcfb24db5d8602872f
bigstock2001 <donald.l.dunn@gmail.com>
2025-08-02 20:20:38 -0500

    Create VideoPlayer.js

---
+++ components/VideoPlayer.js
@@ -0,0 +1,7 @@
+export default function VideoPlayer() {
+  return (
+    <div style={{ backgroundColor: '#000', color: '#fff', padding: '2rem', textAlign: 'center' }}>
+      Video Player Placeholder
+    </div>
+  );
+}
