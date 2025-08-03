3db1a4ef3464959e2ed31a4e7e134629ce816dea
bigstock2001 <donald.l.dunn@gmail.com>
2025-08-03 11:10:02 -0500

    changed video render

--- app/content/page.jsx
+++ app/content/page.jsx
@@ -1,8 +1,7 @@
 'use client';
 
 import { useEffect, useState } from 'react';
-import VideoPlayer from '@/components/VideoPlayer';
-
+import VideoPlayer from '../../components/VideoPlayer';
 export default function ContentPage() {
   const [videos, setVideos] = useState([]);
 
