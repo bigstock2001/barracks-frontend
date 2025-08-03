5f4ca63d9ab75e9b80a71396c48d286aea001e20
bigstock2001 <donald.l.dunn@gmail.com>
2025-08-03 00:29:17 -0500

    Create layout.jsx

---
+++ app/content/layout.jsx
@@ -0,0 +1,8 @@
+export const metadata = {
+  title: "Content | Barracks Media",
+  description: "Watch exclusive content on Barracks Media.",
+};
+
+export default function ContentLayout({ children }) {
+  return <>{children}</>;
+}
