0d3ed1932228197d6fdb6ebead9787e2c4dff6d9
bigstock2001 <donald.l.dunn@gmail.com>
2025-08-07 21:38:44 -0500

    Check git status and force push fixes

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    domains: ['backend.barracksmedia.com', 'images.pexels.com'],
+  },
+  env: {
+    CUSTOM_KEY: 'my-value',
+  },
 }
+
+module.exports = nextConfig
